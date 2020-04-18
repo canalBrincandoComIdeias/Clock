6bdbcfeda96c55c422e3fd565ded5a9bf8b97c0b
canalBrincandoComIdeias <40867558+canalBrincandoComIdeias@users.noreply.github.com>
2020-04-18 14:49:57 -0300

    Controls time counting and comparison
    First Version

---
+++ src/Clock.cpp
@@ -0,0 +1,124 @@
+#include "Arduino.h"
+#include "Clock.h"
+
+Clock::Clock() {
+}
+
+void Clock::ajustaHorario(byte hora, byte minuto, byte segundo) {
+  horario horarioInicial;
+  horarioInicial.segundo = segundo; 
+  horarioInicial.minuto  = minuto; 
+  horarioInicial.hora    = hora;
+  milesimoInicial = horarioParaMilesimo(horarioInicial);
+}
+
+horario Clock::horarioAtual() {  
+  uint32_t milesimoAtual = (milesimoInicial + (millis() % milesimosNoDia));
+  return milesimoParaHorario(milesimoAtual);
+}
+
+bool Clock::horarioMenor(horario valor1, horario valor2) {
+	return horarioParaMilesimo(valor1) < horarioParaMilesimo(valor2);
+}
+
+bool Clock::horarioMaior(horario valor1, horario valor2) {
+	return horarioParaMilesimo(valor1) > horarioParaMilesimo(valor2);
+}
+
+bool Clock::horarioIgual(horario valor1, horario valor2) {  
+	return horarioParaMilesimo(valor1) == horarioParaMilesimo(valor2);
+}
+
+bool Clock::horaMinutoIgual(horario valor1, horario valor2) {  
+	return horarioParaMinutos(valor1) == horarioParaMinutos(valor2);
+}				
+
+horario Clock::adicionaHoras(horario origem, int horas) {
+	uint32_t calculo = horarioParaMilesimo(origem);
+	calculo += (horas * 3600000);	
+	return milesimoParaHorario(calculo);
+}
+
+horario Clock::adicionaMinutos(horario origem, int minutos) {
+	uint32_t calculo = horarioParaMilesimo(origem);
+	calculo += (minutos * 60000);	
+	return milesimoParaHorario(calculo);
+}
+
+horario Clock::adicionaSegundos(horario origem, int segundos) {
+	uint32_t calculo = horarioParaMilesimo(origem);
+	calculo += (segundos * 1000);	
+	return milesimoParaHorario(calculo);
+}
+
+horario Clock::adicionaMilesimos(horario origem, int milesimos) {
+	uint32_t calculo = horarioParaMilesimo(origem);
+	calculo += milesimos;	
+	return milesimoParaHorario(calculo);
+}
+		
+uint32_t Clock::horarioParaMilesimo(horario origem) {
+  uint32_t retorno;
+
+  retorno = horarioParaSegundos(origem) * 1000;
+  return retorno;
+}
+
+uint32_t Clock::horarioParaSegundos(horario origem) {
+  uint32_t retorno;
+
+  retorno = horarioParaMinutos(origem) * 60;
+  retorno += origem.segundo;
+
+  return retorno;
+}
+
+uint32_t Clock::horarioParaMinutos(horario origem) {
+  uint32_t retorno;
+
+  retorno =  (uint32_t)origem.hora * 60; 
+  retorno += (uint32_t)origem.minuto;
+
+  return retorno;
+}
+
+horario Clock::milesimoParaHorario(uint32_t origem) {
+  horario retorno;
+
+  origem = origem / 1000;
+  retorno.segundo = origem % 60;
+  
+  origem = origem / 60;
+  retorno.minuto  = origem % 60;
+
+  origem = origem / 60;
+  retorno.hora  = origem % 24;
+  return retorno;
+}
+
+String Clock::horaParaTexto(horario origem) {
+  String textoRetorno = strZero(origem.hora, 2);
+  textoRetorno += ":";
+  textoRetorno += strZero(origem.minuto, 2);
+  textoRetorno += ":";
+  textoRetorno += strZero(origem.segundo, 2);
+  return textoRetorno;
+}
+
+String Clock::strZero(int valor, byte digitos) {
+  String textoRetorno = String(valor);
+  while (textoRetorno.length() < digitos ) {
+    textoRetorno = "0" + textoRetorno;
+  }
+  return textoRetorno;
+}
+
+void Clock::quandoMillisZerar() {
+static uint32_t ultimoMillis;
+
+  if (millis() < ultimoMillis) { //millis zerou
+     milesimoInicial += (uint32_t)pow(2, sizeof(uint32_t)*8 ) % milesimosNoDia;        
+     milesimoInicial = milesimoInicial % milesimosNoDia;
+  }
+  ultimoMillis = millis();   
+}
---
+++ src/Clock.h
@@ -0,0 +1,50 @@
+//Estrutura e tipo para armazenar um horario
+typedef struct {
+  byte hora;
+  byte minuto;
+  byte segundo;
+} horario;
+
+//Quantos milesimos de segundos um dia possui
+#define milesimosNoDia 86400000
+
+//definição da classe Clock
+class Clock
+{
+  public:
+    Clock();
+	
+	//Acerta o horario (para uso no SETUP)
+	void ajustaHorario(byte hora, byte minuto, byte segundo);
+
+	//Retorna o horario atual
+	horario horarioAtual();
+
+	//Comparações entre dois horarios
+	bool horarioMenor(horario valor1, horario valor2);
+	bool horarioMaior(horario valor1, horario valor2);
+	bool horarioIgual(horario valor1, horario valor2);
+	bool horaMinutoIgual(horario valor1, horario valor2);
+
+	//Adicao
+	horario adicionaHoras(horario origem, int horas);
+	horario adicionaMinutos(horario origem, int minutos);
+	horario adicionaSegundos(horario origem, int segundos);
+    horario adicionaMilesimos(horario origem, int milesimos);
+
+	//Conversão
+	uint32_t horarioParaMilesimo(horario origem);
+	uint32_t horarioParaSegundos(horario origem);
+	uint32_t horarioParaMinutos(horario origem);
+	horario milesimoParaHorario(uint32_t origem);
+	String horaParaTexto(horario origem);
+
+	//Ajuste do relogio quando o millis() zerar (a cada 50 dias aprox.)
+	void quandoMillisZerar();
+
+
+  private:
+    uint32_t milesimoInicial;
+	String strZero(int valor, byte digitos);
+	
+};
